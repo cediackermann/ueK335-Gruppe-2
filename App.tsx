7c3f180b1f1407592115f9f1b6d7b5f826fabf66
cediackermann <ackermanncedi@gmail.com>
2025-06-06 09:47:54 +0200

    add login and logout

--- App.tsx
+++ App.tsx
@@ -1,29 +1,18 @@
 import { Provider } from "react-native-paper";
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./pages/Login";
-import Main from "./pages/Main";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Router from "./Router";
+import queryClient from "./services/QueryClient";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Provider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name='Login'
-            component={Login}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='Main'
-            component={Main}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <Router />
+      </Provider>
+    </QueryClientProvider>
   );
 }
--- tabs/Profile.tsx
+++ tabs/Profile.tsx
@@ -1,5 +1,18 @@
-import { Text } from "react-native-paper";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { logout } from "../services/Auth";
 
 export default function Profile() {
-  return <Text>Profile</Text>;
+  return (
+    <View>
+      <Text variant='headlineLarge'>Profile</Text>
+      <Button
+        onPress={() => {
+          logout();
+        }}
+      >
+        Logout
+      </Button>
+    </View>
+  );
 }
